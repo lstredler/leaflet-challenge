57a11c477ca81bfaf0bc16f53a48ca4db8ffb6d9
Streds <laurenstredler@MacBook-Pro.local>
2021-06-17 18:38:58 -0500

    updates

--- Solved/config.js
+++ Solved/config.js
@@ -1,4 +1,2 @@
 // API key
 const API_KEY = "pk.eyJ1IjoibHN0cmVkbGVyIiwiYSI6ImNrcG9vajFoZTA4ZzAybnFqbm9tZndvNDYifQ.-k7_K_rwQAeJwq6aqxGteA"
-
-//https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
